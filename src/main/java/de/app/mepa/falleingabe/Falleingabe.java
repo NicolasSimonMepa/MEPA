aa5db9dd92d565ba714ffb73d3f9e9c53296c32f
vstumpe <vivien.stumpe@t-online.de>
2016-04-04 12:16:19 +0200

    Update Falleingabe.java
    Navigation zu Persönlichen Daten hergestellt

--- src/main/java/de/app/mepa/falleingabe/Falleingabe.java
+++ src/main/java/de/app/mepa/falleingabe/Falleingabe.java
@@ -1,4 +1,4 @@
-//Zuletzt geändert von Vivien Stumpe am 03.04.16
+//Zuletzt geändert von Vivien Stumpe am 04.04.16
 package de.app.mepa.falleingabe;
 
 import android.content.Intent;
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import de.app.mepa.menu.Menu;
 import de.app.mepa.mepa.R;
+import de.app.mepa.pers_daten.Pers_daten;
 
 //OnClickListener implementieren, um auf einen Klick des Benutzers zu reagieren
 //von Vivien Stumpe, 03.04.16
@@ -16,6 +18,10 @@ public class Falleingabe extends AppCompatActivity implements View.OnClickListen
     //private Buttonvariable, die auf den Button in der Activity zeigen soll
     //von Vivien Stumpe, 03.04.16
     private Button btn_fallein;
+
+    //private Textviewvariable, die auf Persönliche Daten in der Activity zeigen soll
+    //von Vivien Stumpe, 04.04.16
+    private TextView txtv_pers_daten;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,15 @@ public class Falleingabe extends AppCompatActivity implements View.OnClickListen
         //Click Event abfangen und den OnClickListener für die aktuelle View aufrufen
         //von Vivien Stumpe, 03.04.16
         btn_fallein.setOnClickListener(this);
+
+        //Verbindung zwischen Variable und TextView in der Activity herstellen
+        //von Vivien Stumpe, 04.04.16
+        txtv_pers_daten = (TextView)findViewById(R.id.txtv_pers_daten);
+
+        //Click Event abfangen und den OnClickListener für die aktuelle View aufrufen
+        //von Vivien Stumpe, 04.04.16
+        txtv_pers_daten.setOnClickListener(this);
+
     }
 
     @Override
@@ -45,5 +60,14 @@ public class Falleingabe extends AppCompatActivity implements View.OnClickListen
             Intent intent = new Intent(Falleingabe.this, Menu.class);
             startActivity(intent);
         }
+        //Ein Intent erzeugen, wenn der bestimmte Button geklickt wurde
+        //Das Intent stellt eine Verbindung zur angegebenen Activity (Bildschirmseite) her
+        //Aufrufen der Activity mittels Intent
+        //von Vivien Stumpe, 04.04.16
+        if(ce == R.id.txtv_pers_daten){
+            Intent intent = new Intent(Falleingabe.this, Pers_daten.class);
+            startActivity(intent);
+        }
     }
 }
+
