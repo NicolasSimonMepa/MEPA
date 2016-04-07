8ef5248e793e467713f0187acbb61459a97dcc1c
EmileYon <emile.yoncaova@stud.hs-hannover.de>
2016-04-07 17:37:59 +0200

    Update and rename Maßnahmen.java to Massnahmen.java

--- src/main/java/de/app/mepa/massnahmen/Massnahmen.java
+++ src/main/java/de/app/mepa/massnahmen/Massnahmen.java
@@ -1,16 +1,11 @@
 package de.app.mepa.massnahmen;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 
-import de.app.mepa.menu.Menu;
 import de.app.mepa.mepa.R;
 
-public class Maßnahmen extends AppCompatActivity {
+public class Massnahmen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
