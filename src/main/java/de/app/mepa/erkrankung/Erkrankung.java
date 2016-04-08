2323594697e58932ce5d160cd8cc19d305b3b1a6
EmileYon <emile.yoncaova@stud.hs-hannover.de>
2016-04-08 15:38:51 +0200

    Update Erkrankung.java

--- src/main/java/de/app/mepa/erkrankung/Erkrankung.java
+++ src/main/java/de/app/mepa/erkrankung/Erkrankung.java
@@ -1,13 +1,7 @@
 package de.app.mepa.erkrankung;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-
-import de.app.mepa.menu.Menu;
 import de.app.mepa.mepa.R;
 
 public class Erkrankung extends AppCompatActivity {
