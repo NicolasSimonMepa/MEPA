a5597d12b9c65a54bd9682b243837af8fb45daeb
NicolasSimonMepa <nicolas.simon@stud.hs-hannover.de>
2016-04-10 16:16:12 +0200

    Added files via upload

---
+++ src/main/java/de/app/mepa/upload/Upload.java
@@ -0,0 +1,16 @@
+//Zuletzt geändert von Vivien Stumpe, 10.04.16
+package de.app.mepa.upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.app.mepa.mepa.R;
+
+public class Upload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
