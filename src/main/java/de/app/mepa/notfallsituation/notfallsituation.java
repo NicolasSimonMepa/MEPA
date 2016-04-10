d28e64bbe8e9cc0942fc286f2b581fec2b1e0a02
crapmund <christian.rapmund@stud.hs-hannover.de>
2016-04-10 20:54:38 +0200

    Create notfallsituation.java

---
+++ src/main/java/de/app/mepa/notfallsituation/notfallsituation.java
@@ -0,0 +1,15 @@
+package de.app.mepa.notfallsituation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.app.mepa.mepa.R;
+
+public class notfallsituation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notfallsituation);
+    }
+}
