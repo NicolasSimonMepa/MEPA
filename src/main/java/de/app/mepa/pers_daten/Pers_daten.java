4ce7e642a10f0998d640547d2b75f84dbb7c10c9
crapmund <christian.rapmund@stud.hs-hannover.de>
2016-04-10 20:55:21 +0200

    Create Pers_daten.java

---
+++ src/main/java/de/app/mepa/pers_daten/Pers_daten.java
@@ -0,0 +1,15 @@
+package de.app.mepa.pers_daten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.app.mepa.mepa.R;
+
+public class Pers_daten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pers_daten);
+    }
+}
