7f03a7abd508857ba2c11f6892752fb012f1c40a
vstumpe <vivien.stumpe@t-online.de>
2016-04-11 17:56:52 +0200

    Update Pers_daten.java
    Swipe nach links zum Wechseln der Aktivität

--- src/main/java/de/app/mepa/pers_daten/Pers_daten.java
+++ src/main/java/de/app/mepa/pers_daten/Pers_daten.java
@@ -1,16 +1,21 @@
-//Zuletzt bearbeitet von Vivien Stumpe, 10.04.16
+//Zuletzt bearbeitet von Vivien Stumpe, 11.04.16
 package de.app.mepa.pers_daten;
 
 import android.content.Intent;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import de.app.mepa.MyAdapter;
 import de.app.mepa.einstellungen.Einstellungen;
+import de.app.mepa.erkrankung.Erkrankung;
 import de.app.mepa.falleingabe.Falleingabe;
 import de.app.mepa.falluebersicht.Falluebersicht;
 import de.app.mepa.impressum.Impressum;
@@ -18,8 +23,11 @@ import de.app.mepa.mepa.MainActivity;
 import de.app.mepa.mepa.R;
 import de.app.mepa.stammdaten.Stammdaten;
 import de.app.mepa.upload.Upload;
+import de.app.mepa.verletzung.Verletzung;
+import de.app.mepa.OnSwipeTouchListener;
 
-public class Pers_daten extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+public class Pers_daten extends AppCompatActivity implements AdapterView.OnItemClickListener {
 //von Vivien Stumpe, 10.04.16
 //DrawerLayout für das Hamburger Menü
 //ListView, die die Einträge des Menüs enthält
@@ -30,6 +38,10 @@ public class Pers_daten extends AppCompatActivity implements AdapterView.OnItemC
     private MyAdapter myadapter_pers_daten;
     private int[] drawer_icons_pers_daten={R.drawable.mepa_icon, R.drawable.mepa_icon,
             R.drawable.falleingabe, R.drawable.mepa_icon, R.drawable.upload, R.drawable.impressum, R.drawable.mepa_icon,};
+    //von Vivien Stumpe, 11.04.16
+    //View für das Hauptelement der Aktivität - zum Wechseln mittels Swipe
+    private View view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +56,21 @@ public class Pers_daten extends AppCompatActivity implements AdapterView.OnItemC
         listview_pers_daten.setAdapter(myadapter_pers_daten);
         //OnItemClickListener auf die ListView aktivieren, damit auf Klicks reagiert wird
         listview_pers_daten.setOnItemClickListener(this);
+
+        //von Vivien Stumpe, 11.04.16
+        //Verbindung der View zur Scrollview in der Aktivität
+    view=(View) findViewById(R.id.scrV_pers_daten);
+        //OnTouchListener auf die View setzen
+        view.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            //Bei einem Swipe nach links wird die nächste Aktivität geöffnet
+            public void onSwipeLeft() {
+            Intent intent = new Intent(Pers_daten.this, Verletzung.class);
+               startActivity(intent);
+        }
+        });
     }
+
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
