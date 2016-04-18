c1d2073d4935c557650c4bda07232069f0f412e9
Nathalie107 <nathalie.horn@stud.hs-hannover.de>
2016-04-18 14:52:53 +0200

    Added files via upload

---
+++ src/main/java/de/app/mepa/Mitarbeiterkonfig.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Nathalie on 18.04.2016.
+ */
+
+package de.app.mepa.mitarbeiterkonfig;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import de.app.mepa.MyAdapter;
+import de.app.mepa.einstellungen.Einstellungen;
+import de.app.mepa.falleingabe.Falleingabe;
+import de.app.mepa.falluebersicht.Falluebersicht;
+import de.app.mepa.impressum.Impressum;
+import de.app.mepa.mepa.MainActivity;
+import de.app.mepa.mepa.R;
+import de.app.mepa.stammdaten.Stammdaten;
+import de.app.mepa.upload.Upload;
+
+
+//OnClickListener implementieren, um zu reagieren wenn eine View geklickt wurde
+//OnItemClickListener implementieren, um auf einen Klick in der ListView reagieren zu können
+
+public class Mitarbeiterkonfig {}
+
+
+
+
