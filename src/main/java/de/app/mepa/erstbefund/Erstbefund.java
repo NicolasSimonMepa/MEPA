6858c476c13059e34a89ee48b1278e850203dac3
EmileYon <emile.yoncaova@stud.hs-hannover.de>
2016-04-26 08:55:08 +0200

    Update Erstbefund.java

--- src/main/java/de/app/mepa/erstbefund/Erstbefund.java
+++ src/main/java/de/app/mepa/erstbefund/Erstbefund.java
@@ -5,8 +5,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -19,12 +21,12 @@ import de.app.mepa.falleingabe.Falleingabe;
 import de.app.mepa.falluebersicht.Falluebersicht;
 import de.app.mepa.impressum.Impressum;
 import de.app.mepa.massnahmen.Massnahmen;
-import de.app.mepa.mepa.MainActivity;
 import de.app.mepa.mepa.R;
-import de.app.mepa.stammdaten.Stammdaten;
 import de.app.mepa.upload.Upload;
 
-public class Erstbefund extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener{
+public class Erstbefund extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener, View.OnClickListener{
+        private ImageView imgv_before;
+        private ImageView imgv_next;
         private Spinner spin_bewusstseinslage;
         private Spinner spin_kreislauf;
         private Spinner spin_pupillenfunktion_rechts;
@@ -44,8 +46,8 @@ public class Erstbefund extends AppCompatActivity implements AdapterView.OnItemS
         private DrawerLayout drawerlayout_erstbefund;
         private ListView listview_erstbefund;
         private MyAdapter myadapter_erstbefund;
-         private int[] drawer_icons_erstbefund={R.drawable.mepa_icon, R.drawable.einstellungen,
-            R.drawable.falleingabe, R.drawable.falluebersicht, R.drawable.upload, R.drawable.impressum, R.drawable.stammdaten,};
+         private int[] drawer_icons_erstbefund={R.drawable.falleingabe,
+                 R.drawable.falluebersicht, R.drawable.upload, R.drawable.einstellungen, R.drawable.impressum};
         private ActionBarDrawerToggle actionbardrawertoggle;
         Toolbar toolbar;
 
@@ -129,7 +131,7 @@ protected void onCreate(Bundle savedInstanceState){
         drawerlayout_erstbefund=(DrawerLayout)findViewById(R.id.drawerLayout_Erstbefund);
         listview_erstbefund=(ListView)findViewById(R.id.listview_erstbefund);
         //Adapter erzeugen und setzen, um die Einträge der ListView darzustellen
-        myadapter_erstbefund=new MyAdapter(this,this.getResources().getStringArray(R.array.drawer_nav),drawer_icons_erstbefund);
+        myadapter_erstbefund=new MyAdapter(this,this.getResources().getStringArray(R.array.drawer_nav_neu),drawer_icons_erstbefund);
         listview_erstbefund.setAdapter(myadapter_erstbefund);
         //OnItemClickListener auf die ListView aktivieren, damit auf Klicks reagiert wird
         listview_erstbefund.setOnItemClickListener(this);
@@ -153,7 +155,12 @@ protected void onCreate(Bundle savedInstanceState){
                 startActivity(intent);
             }
         });
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
+        imgv_before = (ImageView)findViewById(R.id.imgv_before_erstbefund);
+        imgv_next = (ImageView)findViewById(R.id.imgv_next_erstbefund);
+        imgv_before.setOnClickListener(this);
+        imgv_next.setOnClickListener(this);
         }
 
 @Override
@@ -193,39 +200,51 @@ public void onItemClick(AdapterView<?>parent,View view,int position,long id){
 
 private void selectItemFromDrawer(int position){
         //Wenn das erste Element im Menü geklickt wurde, wird zurück zum Start navigiert
-        if(position==0){
-        Intent intent=new Intent(Erstbefund.this,MainActivity.class);
-        startActivity(intent);
+        if(position==0) {
+                Intent intent = new Intent(Erstbefund.this, Falleingabe.class);
+                startActivity(intent);
         }
         //Wenn das zweite Element im Menü geklickt wurde, werden die Einstellungen aufgerufen
-        if(position==1){
-        Intent intent=new Intent(Erstbefund.this,Einstellungen.class);
-        startActivity(intent);
+        if(position==1) {
+                Intent intent = new Intent(Erstbefund.this, Einstellungen.class);
+                startActivity(intent);
         }
         //Wenn das dritte Element im Menü geklickt wurde, wird die Falleingabe aufgerufen
-        if(position==2){
-        Intent intent=new Intent(Erstbefund.this,Falleingabe.class);
-        startActivity(intent);
+        if(position==2) {
+                Intent intent = new Intent(Erstbefund.this, Falluebersicht.class);
+                startActivity(intent);
         }
         //Wenn das vierte Element im Menü geklickt wurde, wird die Fallübersicht geöffnet
-        if(position==3){
-        Intent intent=new Intent(Erstbefund.this,Falluebersicht.class);
-        startActivity(intent);
+        if(position==3) {
+                Intent intent = new Intent(Erstbefund.this, Upload.class);
+                startActivity(intent);
         }
         //Wenn das fünfte Element im Menü geklickt wurde, wird der Upload geöffnet
-        if(position==4){
-        Intent intent=new Intent(Erstbefund.this,Upload.class);
-        startActivity(intent);
-        }
-        //Wenn das sechste Element im Menü geklickt wurde, wird das Impressum geöffnet
-        if(position==5){
-        Intent intent=new Intent(Erstbefund.this,Impressum.class);
-        startActivity(intent);
-        }
-        //Wenn das siebte Element im Menü geklickt wurde, werden die Stammdaten geöffnet
-        if(position==6){
-        Intent intent=new Intent(Erstbefund.this,Stammdaten.class);
-        startActivity(intent);
+        if(position==4) {
+                Intent intent = new Intent(Erstbefund.this, Impressum.class);
+                startActivity(intent);
         }
         }
+        @Override
+        public void onClick(View v) {
+        /* von Vivien Stumpe, 25.04.16
+        Deklaration und Initialisierung einer Hilfsvariablen (clicked element),
+        die die ID der geklickten View erhält
+        */
+                int ce = v.getId();
+
+        /* von Vivien Stumpe, 25.04.16
+        Ein Intent erzeugen, wenn die bestimmte ImageView geklickt wurde
+        Das Intent stellt eine Verbindung zur angegebenen Activity (Bildschirmseite) her
+        Aufrufen der Activity mittels Intent
+        */
+                if (ce == R.id.imgv_before_erstbefund) {
+                        Intent intent = new Intent(Erstbefund.this, Massnahmen.class);
+                        startActivity(intent);
+                }
+                if (ce == R.id.imgv_next_erstbefund) {
+                        Intent intent = new Intent(Erstbefund.this, Ersthelfermassnahmen.class);
+                        startActivity(intent);
+                }
         }
+}
