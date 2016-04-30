d3ab6236a500dae02fd93e8edb46ab0e94eae4e2
NicolasSimonMepa <nicolas.simon@stud.hs-hannover.de>
2016-04-30 22:36:24 +0200

    Update Falluebersicht.java

--- src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
+++ src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
@@ -1,4 +1,4 @@
-//Zuletzt geändert von Nicoals Simon 17.04.16
+//Zuletzt geändert von Nicoals Simon 30.04.16
 package de.app.mepa.falluebersicht;
 
 import android.content.Intent;
@@ -17,20 +17,18 @@ import de.app.mepa.MyAdapter;
 import de.app.mepa.einstellungen.Einstellungen;
 import de.app.mepa.falleingabe.Falleingabe;
 import de.app.mepa.impressum.Impressum;
-import de.app.mepa.mepa.MainActivity;
 import de.app.mepa.mepa.R;
-import de.app.mepa.stammdaten.Stammdaten;
 import de.app.mepa.upload.Upload;
 
 public class Falluebersicht extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
     //Button-Variablen für die Buttons in der Einstellungen Activity
-    //eingefügt von Nicolas Simon 17.04.16
+    //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
 
 
     private LinearLayout lnl_buttons;
 
-   //eingefügt von Nicolas Simon 17.04.16
+   //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
     //DrawerLayout für das Hamburger Menü
     //ListView, die die Einträge des Menüs enthält
     //Adapter, der die Einträge der ListView darstellt
@@ -41,7 +39,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
     private int[] drawer_icons_falluebersicht={R.drawable.mepa_icon, R.drawable.einstellungen,R.drawable.falleingabe,
             R.drawable.falluebersicht, R.drawable.upload, R.drawable.impressum, R.drawable.stammdaten};
 
-    /*von Vivien Stumpe, 12.04.16
+    /*Nicolas Simon, übernommen von Vivien Stumpe, 12.04.16
     Der ActionBarDrawerToggle sorgt dafür, dass das DrawerLayout in der übergebenen Toolbar angezeigt wird
     ActionBarDrawerToggle und Toolbar anlegen
     */
@@ -69,7 +67,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
         btn_speichern_fallueb.setOnClickListener(this);
 
 
-        //eingefügt von Nicolas Simon 17.04.16
+        //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
         //zuweisen des Drawers und der ListView zu den Elementen in der xml Datei
         drawerlayout_falluebersicht=(DrawerLayout) findViewById(R.id.drawerLayout_Falluebersicht);
         listview_falluebersicht=(ListView) findViewById(R.id.listview_falluebersicht);
@@ -100,7 +98,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         //clicked element mit dem geklickten Button belegen
-        //von Nicoals Simon, 17.04.16
+        //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
         int ce = v.getId();
         //Ein Intent erzeugen, wenn der Button geklickt wurde
         //Das Intent stellt eine Verbindung zur angegebenen Activity (Bildschirmseite) her und ruft diese auf
@@ -118,7 +116,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
             lnl_buttons.invalidate();
         }
     }
-    //Von Nicolas Simon, 17.04.16
+    //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         //Aufruf der Prozedur mit Übergabe der Position des geklickten Items/Menüpunkt
@@ -127,38 +125,29 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
 
     private void selectItemFromDrawer(int position){
         //Wenn das erste Element im Menü geklickt wurde, wird zurück zum Start navigiert
+        //Wenn das erste Element im Menü geklickt wurde, wird zurück zum Start navigiert
         if(position==0) {
-            Intent intent = new Intent(Falluebersicht.this, MainActivity.class);
-            startActivity(intent);
-        }
-        //Wenn das zweite Element im Menü geklickt wurde, werden die Einstellungen aufgerufen
-        if(position==1) {
-            Intent intent = new Intent(Falluebersicht.this, Einstellungen.class);
-            startActivity(intent);
-        }
-        //Wenn das dritte Element im Menü geklickt wurde, wird die Falleingabe aufgerufen
-        if(position==2) {
             Intent intent = new Intent(Falluebersicht.this, Falleingabe.class);
             startActivity(intent);
         }
-        //Wenn das vierte Element im Menü geklickt wurde, wird die Fallübersicht geöffnet
-        if(position==3) {
+        //Wenn das zweite Element im Menü geklickt wurde, wird die Falluebersicht aufgerufen
+        if(position==1) {
             Intent intent = new Intent(Falluebersicht.this, Falluebersicht.class);
             startActivity(intent);
         }
-        //Wenn das fünfte Element im Menü geklickt wurde, wird der Upload geöffnet
-        if(position==4) {
+        //Wenn das dritte Element im Menü geklickt wurde, wird der Upload geöffnet
+        if(position==2) {
             Intent intent = new Intent(Falluebersicht.this, Upload.class);
             startActivity(intent);
         }
-        //Wenn das sechste Element im Menü geklickt wurde, wird das Impressum geöffnet
-        if(position==5) {
-            Intent intent = new Intent(Falluebersicht.this, Impressum.class);
+        //Wenn das vierte Element im Menü geklickt wurde, werden die Einstellungen geöffnet
+        if(position==3) {
+            Intent intent = new Intent(Falluebersicht.this, Einstellungen.class);
             startActivity(intent);
         }
-        //Wenn das siebte Element im Menü geklickt wurde, werden die Stammdaten geöffnet
-        if(position==6) {
-            Intent intent = new Intent(Falluebersicht.this, Stammdaten.class);
+        //Wenn das fünfte Element im Menü geklickt wurde, wird das Impressum geöffnet
+        if(position==4) {
+            Intent intent = new Intent(Falluebersicht.this, Impressum.class);
             startActivity(intent);
         }
 
