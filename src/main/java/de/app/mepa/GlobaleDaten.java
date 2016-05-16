318e9ef459b1beedd61e518a33aa92487a41e1ab
vstumpe <vivien.stumpe@t-online.de>
2016-05-16 18:25:09 +0200

    Update GlobaleDaten.java

--- src/main/java/de/app/mepa/GlobaleDaten.java
+++ src/main/java/de/app/mepa/GlobaleDaten.java
@@ -1,3 +1,4 @@
+//Zuletzt bearbeitet von Vivien Stumpe, 16.05.16
 package de.app.mepa;
 
 import android.app.Application;
@@ -27,6 +28,30 @@ public class GlobaleDaten extends Application {
     //Einsatz - Attribute
     private String ein_zugef;
     private String ein_fundort;
+    private Integer ein_mosan, ein_hilfs, ein_sanw;
+
+    //Stammdaten - Attribute
+    //wurden die Stammdaten für den Fall angelegt?
+    private Boolean fall_angelegt=false;
+    //soll der OnBoarding Prozess übersprungen werden?
+    private Boolean uebersprungen=false;
+
+    //Sanitäter - Attribute
+    private String san_name;
+    private String san_vname;
+    private Boolean san_vorh=false;
+
+    //Verband - Attribute
+    private String verb_kreisv;
+    private String verb_ortsv;
+    private Boolean verb_vorh=false;
+
+    //Veranstaltung - Attribute
+    private String ver_name;
+    private String ver_ort;
+    private String ver_date;
+    private Boolean ver_vorh=false;
+
     //Patient - Dienste
     public void setPat_name(String n){
         this.pat_name=n;
@@ -135,8 +160,135 @@ public class GlobaleDaten extends Application {
     public String getEin_fundort(){
         return ein_fundort;
     }
+    public void setEin_mosan(Integer m){
+        this.ein_mosan=m;
+    }
+    public Integer getEin_mosan(){
+        return ein_mosan;
+    }
+    public void setEin_hilfs(Integer h){
+        this.ein_hilfs=h;
+    }
+    public Integer getEin_hilfs(){
+        return ein_hilfs;
+    }
+    public void setEin_sanw(Integer s){
+        this.ein_sanw=s;
+    }
+    public Integer getEin_sanw(){
+        return ein_sanw;
+    }
     public void loescheEin(){
         setEin_zugef("");
         setEin_fundort("");
+        setEin_hilfs(0);
+        setEin_sanw(0);
+        setEin_mosan(0);
     }
+
+    //Stammdaten - Dienste
+    public void setFall_angelegt(){
+        if(ver_vorh&san_vorh&verb_vorh) {
+            this.fall_angelegt = true;
+        }
+    }
+    public Boolean getFall_angelegt(){
+        return fall_angelegt;
+    }
+    public void setUebersprungen(Boolean u){
+        this.uebersprungen=u;
+    }
+    public Boolean getUebersprungen(){
+        return uebersprungen;
+    }
+
+    //Sanitäter - Dienste
+    public void setSan_name(String n){
+        this.san_name=n;
+    }
+    public String getSan_name(){
+        return san_name;
+    }
+    public void setSan_vorname(String v){
+        this.san_vname=v;
+    }
+    public String getSan_vorname(){
+        return san_vname;
+    }
+    public void setSan_vorh(){
+        if((getSan_name().length()>0)&(getSan_vorname().length()>0)){
+            san_vorh=true;
+        }
+    }
+    public Boolean getSan_vorh(){
+        return san_vorh;
+    }
+
+    public void loescheSan(){
+        setSan_name("");
+        setSan_vorname("");
+        setSan_vorh();
+    }
+    //Verband - Dienste
+    public void setVerb_kreisv(String k){
+        this.verb_kreisv=k;
+    }
+    public String getVerb_kreisv(){
+        return verb_kreisv;
+    }
+    public void setVerb_ortsv(String o){
+        this.verb_ortsv=o;
+    }
+    public String getVerb_ortsv(){
+        return verb_ortsv;
+    }
+    public void setVerb_vorh(){
+        if((getVerb_kreisv().length()>0)&(getVerb_ortsv().length()>0)){
+            verb_vorh=true;
+        }
+    }
+    public Boolean getVerb_vorh(){
+        return verb_vorh;
+    }
+    public void loescheVerb(){
+        setVerb_ortsv("");
+        setVerb_kreisv("");
+        setVerb_vorh();
+    }
+
+    //Veranstaltung - Dienste
+    public void setVer_name(String n){
+        this.ver_name=n;
+    }
+    public String getVer_name(){
+        return ver_name;
+    }
+    public void setVer_ort(String o){
+        this.ver_ort=o;
+    }
+    public String getVer_ort(){
+        return ver_ort;
+    }
+    public void setVer_date(String d){
+        this.ver_date=d;
+    }
+    public void loescheVer(){
+        setVer_name("");
+        setVer_ort("");
+        setVer_date("");
+        setVer_vorh();
+    }
+    public String getVer_date(){
+        return ver_date;
+    }
+    public void setVer_vorh(){
+        if((getVer_name().length()>0)&(getVer_date().length()>0)){
+            ver_vorh=true;
+        }
+    }
+    public Boolean getVer_vorh(){
+        return ver_vorh;
+    }
+
+
 }
