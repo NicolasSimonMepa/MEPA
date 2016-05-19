bdfe0b62243983062771f9818c09bbe30dbcc27d
EmileYon <emile.yoncaova@stud.hs-hannover.de>
2016-05-19 17:37:00 +0200

    Update Erstbefund.java

--- src/main/java/de/app/mepa/erstbefund/Erstbefund.java
+++ src/main/java/de/app/mepa/erstbefund/Erstbefund.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.support.v7.widget.Toolbar;
+
+import de.app.mepa.GlobaleDaten;
 import de.app.mepa.MyAdapter;
 import de.app.mepa.OnSwipeTouchListener;
 import de.app.mepa.einstellungen.Einstellungen;
@@ -34,6 +37,13 @@ public class Erstbefund extends AppCompatActivity implements AdapterView.OnItemS
         private Spinner spin_ekg;
         private Spinner spin_schmerzen;
         private Spinner spin_atmung;
+        private EditText edtxt_rr_systolisch;
+        private EditText edtxt_rr_diastolisch;
+        private EditText edtxt_puls;
+        private EditText edtxt_atemfrequenz;
+        private EditText edtxt_spo2;
+        private EditText edtxt_blutzucker;
+        private GlobaleDaten mfall;
         
         private String[]bewusstseinslage={"-----","orientiert","getrübt","bewusstlos"};
         private String[]kreislauf={"-----","schock","kreislaufstillstand","puls regelmäßig","puls unregelmäßig"};
@@ -127,6 +137,14 @@ protected void onCreate(Bundle savedInstanceState){
         spin_atmung.setAdapter(adapter_atmung);
         spin_atmung.setOnItemSelectedListener(this);
 
+        edtxt_rr_systolisch=(EditText)findViewById(R.id.edtxt_rr_systolisch);
+        edtxt_rr_diastolisch=(EditText)findViewById(R.id.edtxt_rr_diastolisch);
+        edtxt_puls=(EditText)findViewById(R.id.edtxt_puls);
+        edtxt_atemfrequenz=(EditText)findViewById(R.id.edtxt_atemfrequenz);
+        edtxt_spo2=(EditText)findViewById(R.id.edtxt_spo2);
+        edtxt_blutzucker=(EditText)findViewById(R.id.edtxt_blutzucker);
+
+        setWerte();
 
         //zuweisen des Drawers und der ListView zu den Elementen in der xml Datei
         drawerlayout_erstbefund=(DrawerLayout)findViewById(R.id.drawerLayout_Erstbefund);
@@ -246,4 +264,221 @@ private void selectItemFromDrawer(int position){
                 drawerlayout_erstbefund.openDrawer(GravityCompat.START);
             }
         }
+    public void onPause(){
+        super.onPause();
+        //Eingaben werden lokal gespeichert
+        speichereEingaben();
+    }
+    public void speichereEingaben(){
+        mfall=(GlobaleDaten)getApplication();
+        if((spin_bewusstseinslage.getSelectedItem().toString().equals("orientiert"))){
+            mfall.setErst_bewusstsein("orientiert");
+        }
+        if((spin_bewusstseinslage.getSelectedItem().toString().equals("getrübt"))){
+            mfall.setErst_bewusstsein("getrübt");
+        }
+        if((spin_bewusstseinslage.getSelectedItem().toString().equals("bewusstlos"))){
+            mfall.setErst_bewusstsein("bewusstlos");
+        }
+        if((spin_kreislauf.getSelectedItem().toString().equals("schock"))){
+            mfall.setErst_kreislauf("schock");
+        }
+        if((spin_kreislauf.getSelectedItem().toString().equals("kreislaufstillstand"))){
+            mfall.setErst_kreislauf("kreislaufstillstand");
+        }
+        if((spin_kreislauf.getSelectedItem().toString().equals("puls regelmäßig"))){
+            mfall.setErst_kreislauf("puls regelmäßig");
+        }
+        if((spin_kreislauf.getSelectedItem().toString().equals("puls unregelmäßig"))){
+            mfall.setErst_kreislauf("Puls unregelmäßig");
+        }
+        if((spin_pupillenfunktion_links.getSelectedItem().toString().equals("eng"))){
+            mfall.setErst_pupille_li("eng");
+        }
+        if((spin_pupillenfunktion_links.getSelectedItem().toString().equals("mittel"))){
+            mfall.setErst_pupille_li("mittel");
+        }
+        if((spin_pupillenfunktion_links.getSelectedItem().toString().equals("weit"))){
+            mfall.setErst_pupille_li("weit");
+        }
+        if((spin_pupillenfunktion_links.getSelectedItem().toString().equals("entrundet"))){
+            mfall.setErst_pupille_li("entrundet");
+        }
+        if((spin_pupillenfunktion_links.getSelectedItem().toString().equals("lichtreaktion"))){
+            mfall.setErst_pupille_li("lichtreaktion");
+        }
+        if((spin_pupillenfunktion_rechts.getSelectedItem().toString().equals("eng"))){
+            mfall.setErst_pupille_re("eng");
+        }
+        if((spin_pupillenfunktion_rechts.getSelectedItem().toString().equals("mittel"))){
+            mfall.setErst_pupille_re("mittel");
+        }
+        if((spin_pupillenfunktion_rechts.getSelectedItem().toString().equals("weit"))){
+            mfall.setErst_pupille_re("weit");
+        }
+        if((spin_pupillenfunktion_rechts.getSelectedItem().toString().equals("entrundet"))){
+            mfall.setErst_pupille_re("entrundet");
+        }
+        if((spin_pupillenfunktion_rechts.getSelectedItem().toString().equals("lichtreaktion"))){
+            mfall.setErst_pupille_re("lichtreaktion");
+        }
+        if((spin_ekg.getSelectedItem().toString().equals("sinusrhythmus"))){
+            mfall.setErst_ekg("sinusrhythmus");
+        }
+        if((spin_ekg.getSelectedItem().toString().equals("rhythmusstörung"))){
+            mfall.setErst_ekg("rhythmusstörung");
+        }
+        if((spin_ekg.getSelectedItem().toString().equals("kammerflimmern"))){
+            mfall.setErst_ekg("kammerflimmern");
+        }
+        if((spin_ekg.getSelectedItem().toString().equals("asystolie"))){
+            mfall.setErst_ekg("asystolie");
+        }
+        if((spin_schmerzen.getSelectedItem().toString().equals("leicht"))){
+            mfall.setErst_schmerzen("leicht");
+        }
+        if((spin_schmerzen.getSelectedItem().toString().equals("mittel"))){
+            mfall.setErst_schmerzen("mittel");
+        }
+        if((spin_schmerzen.getSelectedItem().toString().equals("stark"))){
+            mfall.setErst_schmerzen("stark");
+        }
+        if((spin_atmung.getSelectedItem().toString().equals("spontan/frei"))){
+            mfall.setErst_atmung("spontan/frei");
+        }
+        if((spin_atmung.getSelectedItem().toString().equals("atemnot"))){
+            mfall.setErst_atmung("atemnot");
+        }
+        if((spin_atmung.getSelectedItem().toString().equals("hyperventilation"))){
+            mfall.setErst_atmung("hyperventilation");
+        }
+        if((spin_atmung.getSelectedItem().toString().equals("atemstillstand"))){
+            mfall.setErst_atmung("atemstillstand");
+        }
+        mfall.setErst_rr_sys(edtxt_rr_systolisch.getText().toString());
+        mfall.setErst_rr_dia(edtxt_rr_diastolisch.getText().toString());
+        mfall.setErst_puls(edtxt_puls.getText().toString());
+        mfall.setErst_af(edtxt_atemfrequenz.getText().toString());
+        mfall.setErst_spo(edtxt_spo2.getText().toString());
+        mfall.setErst_bz(edtxt_blutzucker.getText().toString());
+    }
+    public void setWerte() {
+        mfall = (GlobaleDaten) getApplication();
+        if((mfall.getErst_bewusstsein()!=null)){
+            if(mfall.getErst_bewusstsein().equals("orientiert")){
+                spin_bewusstseinslage.setSelection(1);
+            }
+            if(mfall.getErst_bewusstsein().equals("getrübt")){
+                spin_bewusstseinslage.setSelection(2);
+            }
+            if(mfall.getErst_bewusstsein().equals("bewusstlos")){
+                spin_bewusstseinslage.setSelection(3);
+            }
+        }
+        if((mfall.getErst_kreislauf()!=null)){
+            if(mfall.getErst_kreislauf().equals("schock")){
+                spin_kreislauf.setSelection(1);
+            }
+            if(mfall.getErst_kreislauf().equals("kreislaufstillstand")){
+                spin_kreislauf.setSelection(2);
+            }
+            if(mfall.getErst_kreislauf().equals("puls regelmäßig")){
+                spin_kreislauf.setSelection(3);
+            }
+            if(mfall.getErst_kreislauf().equals("puls unregelmäßig")){
+                spin_kreislauf.setSelection(4);
+            }
+        }
+        if((mfall.getErst_pupille_li()!=null)){
+            if(mfall.getErst_pupille_li().equals("eng")){
+                spin_pupillenfunktion_links.setSelection(1);
+            }
+            if(mfall.getErst_pupille_li().equals("mittel")){
+                spin_pupillenfunktion_links.setSelection(2);
+            }
+            if(mfall.getErst_pupille_li().equals("weit")){
+                spin_pupillenfunktion_links.setSelection(3);
+            }
+            if(mfall.getErst_pupille_li().equals("entrundet")){
+                spin_pupillenfunktion_links.setSelection(4);
+            }
+            if(mfall.getErst_pupille_li().equals("lichtreaktion")){
+                spin_pupillenfunktion_links.setSelection(5);
+            }
+        }
+        if((mfall.getErst_pupille_re()!=null)){
+            if(mfall.getErst_pupille_re().equals("eng")){
+                spin_pupillenfunktion_rechts.setSelection(1);
+            }
+            if(mfall.getErst_pupille_re().equals("mittel")){
+                spin_pupillenfunktion_rechts.setSelection(2);
+            }
+            if(mfall.getErst_pupille_re().equals("weit")){
+                spin_pupillenfunktion_rechts.setSelection(3);
+            }
+            if(mfall.getErst_pupille_re().equals("entrundet")){
+                spin_pupillenfunktion_rechts.setSelection(4);
+            }
+            if(mfall.getErst_pupille_re().equals("lichtreaktion")){
+                spin_pupillenfunktion_rechts.setSelection(5);
+            }
+        }
+        if((mfall.getErst_ekg()!=null)){
+            if(mfall.getErst_ekg().equals("sinusrhythmus")){
+                spin_ekg.setSelection(1);
+            }
+            if(mfall.getErst_ekg().equals("rhythmusstörung")){
+                spin_ekg.setSelection(2);
+            }
+            if(mfall.getErst_ekg().equals("kammerflimmern")){
+                spin_ekg.setSelection(3);
+            }
+            if(mfall.getErst_ekg().equals("asystolie")){
+                spin_ekg.setSelection(4);
+            }
+        }
+        if((mfall.getErst_schmerzen()!=null)){
+            if(mfall.getErst_schmerzen().equals("leicht")){
+                spin_schmerzen.setSelection(1);
+            }
+            if(mfall.getErst_schmerzen().equals("mittel")){
+                spin_schmerzen.setSelection(2);
+            }
+            if(mfall.getErst_schmerzen().equals("stark")){
+                spin_schmerzen.setSelection(3);
+            }
+        }
+        if((mfall.getErst_atmung()!=null)){
+            if(mfall.getErst_atmung().equals("spontan/frei")){
+                spin_atmung.setSelection(1);
+            }
+            if(mfall.getErst_atmung().equals("atemnot")){
+                spin_atmung.setSelection(2);
+            }
+            if(mfall.getErst_atmung().equals("hyperventilation")){
+                spin_atmung.setSelection(3);
+            }
+            if(mfall.getErst_atmung().equals("atemstillstand")){
+                spin_atmung.setSelection(4);
+            }
+        }
+        if ((mfall.getErst_rr_sys() != null)) {
+            edtxt_rr_systolisch.setText(mfall.getErst_rr_sys());
+        }
+        if ((mfall.getErst_rr_dia() != null)) {
+            edtxt_rr_diastolisch.setText(mfall.getErst_rr_dia());
+        }
+        if ((mfall.getErst_puls() != null)) {
+            edtxt_puls.setText(mfall.getErst_puls());
+        }
+        if ((mfall.getErst_af() != null)) {
+            edtxt_atemfrequenz.setText(mfall.getErst_af());
+        }
+        if ((mfall.getErst_spo() != null)) {
+            edtxt_spo2.setText(mfall.getErst_spo());
+        }
+        if ((mfall.getErst_bz() != null)) {
+            edtxt_blutzucker.setText(mfall.getErst_bz());
+        }
+    }
 }
