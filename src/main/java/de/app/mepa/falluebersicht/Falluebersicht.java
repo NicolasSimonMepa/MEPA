118108c7fabaecd5574a47e0e5780b0b3af4325b
vstumpe <vivien.stumpe@t-online.de>
2016-05-21 19:26:55 +0200

    Update Falluebersicht.java

--- src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
+++ src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
@@ -1,5 +1,6 @@
-//Zuletzt geändert von Nicoals Simon 30.04.16
+//Zuletzt geändert von Vivien Stumpe 21.05.16
 package de.app.mepa.falluebersicht;
+
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,12 +8,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import de.app.mepa.Adapter_Falluebersicht;
 import de.app.mepa.MyAdapter;
 import de.app.mepa.einstellungen.Einstellungen;
 import de.app.mepa.falleingabe.Falleingabe;
@@ -50,22 +56,36 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
 
     private Button btn_speichern_fallueb;
     private Button btn_loeschen_fallueb;
-   /* Vivien Stumpe, 01.05.16
-   doppelt deklariert:
-    private LinearLayout lnl_buttons;
-     */
+
+    /* Vivien Stumpe, 21.05.16
+    String Array mit Testdaten, die in der ListView dargestellt werden sollen
+    Adapter vom Typ Adapter_Falluebersicht, damit die Daten auch dargestellt werden können
+    */
+   String [] faelleArray = {
+           "12343 Müller, Peter",
+           "12234 Schulze, Klaus",
+           "13542 Meier Hans",
+           "16532 Wolf Michael",
+           "17532 Blümel Petra",
+           "14785 Kalend Tina",
+           "14674 Fels Sophie",
+           "36743 Nutella Lisa",
+           "23454 Ulten Ute"
+   };
+    ListView falluebersichtListView;
+    Adapter_Falluebersicht faelleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_falluebersicht);
         //zuweisen des Drawers und der ListView zu den Elementen in der xml Datei
-        drawerlayout_falluebersicht=(DrawerLayout) findViewById(R.id.drawerLayout_Falluebersicht);
-        listview_falluebersicht=(ListView) findViewById(R.id.listview_falluebersicht);
+        drawerlayout_falluebersicht = (DrawerLayout) findViewById(R.id.drawerLayout_Falluebersicht);
+        listview_falluebersicht = (ListView) findViewById(R.id.listview_falluebersicht);
         //Zuweisen der Button-Variablen zu den Buttons in der Activity
         //von Vivien Stumpe, 01.04.16, eingefügt von Nicolas Simon 12.04.16
-        btn_loeschen_fallueb = (Button)findViewById(R.id.btn_loeschen_fallueb);
-        btn_speichern_fallueb = (Button)findViewById(R.id.btn_speichern_fallueb);
+        btn_loeschen_fallueb = (Button) findViewById(R.id.btn_loeschen_fallueb);
+        btn_speichern_fallueb = (Button) findViewById(R.id.btn_speichern_fallueb);
         // Events abfangen und an den OnClickListener die aktuelle View übergeben
         //von Vivien Stumpe, 01.04.16, eingefügt von Nicolas Simon 12.04.16
         btn_loeschen_fallueb.setOnClickListener(this);
@@ -74,10 +94,10 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
 
         //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
         //zuweisen des Drawers und der ListView zu den Elementen in der xml Datei
-        drawerlayout_falluebersicht=(DrawerLayout) findViewById(R.id.drawerLayout_Falluebersicht);
-        listview_falluebersicht=(ListView) findViewById(R.id.listview_falluebersicht);
+        drawerlayout_falluebersicht = (DrawerLayout) findViewById(R.id.drawerLayout_Falluebersicht);
+        listview_falluebersicht = (ListView) findViewById(R.id.listview_falluebersicht);
         //Adapter erzeugen und setzen, um die Einträge der ListView darzustellen
-        myadapter_falluebersicht=new MyAdapter(this, this.getResources().getStringArray(R.array.drawer_nav_neu), drawer_icons_falluebersicht);
+        myadapter_falluebersicht = new MyAdapter(this, this.getResources().getStringArray(R.array.drawer_nav_neu), drawer_icons_falluebersicht);
         listview_falluebersicht.setAdapter(myadapter_falluebersicht);
         //OnItemClickListener auf die ListView aktivieren, damit auf Klicks reagiert wird
         listview_falluebersicht.setOnItemClickListener(this);
@@ -87,10 +107,42 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
         ActionBarDrawerToggle initialisieren
         DrawerListener setzen, damit registriert wird, welchen Status der Drawer hat
         */
-        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        actionbardrawertoggle=new ActionBarDrawerToggle(this, drawerlayout_falluebersicht, toolbar, R.string.drawer_open, R.string.drawer_close);
+        actionbardrawertoggle = new ActionBarDrawerToggle(this, drawerlayout_falluebersicht, toolbar, R.string.drawer_open, R.string.drawer_close);
         drawerlayout_falluebersicht.addDrawerListener(actionbardrawertoggle);
+
+
+        /* von Vivien Stumpe, 21.05.16
+        Die Listview wird mit den Fällen befüllt
+        Die Einträge in der ListView sind klickbar (Löschen Icon siehe Adapter_Falluebersicht.java)
+        Wird ein Eintrag geklickt gelangt man zur Falleingabe und die gespeicherten Daten werden angezeigt
+         */
+        showAllListEntries();
+        falluebersichtListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent fall=new Intent(Falluebersicht.this, Falleingabe.class);
+                ladeFallDaten(position);
+                startActivity(fall);
+
+                Log.d("Fall", id+" ID");
+            }
+        });
+    }
+    /* von Vivien Stumpe, 21.05.16
+    Prozedur, die alle Fälle aus der DB liest und in der ListView darstellt
+    !!Muss noch angepasst werden, wenn Daten aus der DB kommen!
+     */
+    private void showAllListEntries() {
+        List<String> fallListe = new ArrayList<>(Arrays.asList(faelleArray));
+        faelleAdapter =
+                new Adapter_Falluebersicht(
+                        Falluebersicht.this, // Die aktuelle Umgebung (diese Activity)
+                        R.layout.falluebersicht_listview_item, // ID der XML-Layout Datei
+                        faelleArray); // Beispieldaten in einer ArrayList
+        falluebersichtListView = (ListView)findViewById(R.id.list_falluebersicht);
+        falluebersichtListView.setAdapter(faelleAdapter);
     }
 
     @Override
@@ -108,7 +160,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
         //Ein Intent erzeugen, wenn der Button geklickt wurde
         //Das Intent stellt eine Verbindung zur angegebenen Activity (Bildschirmseite) her und ruft diese auf
         //eingefügt von Nicolas Simon 17.04.16
-       if (ce == R.id.btn_speichern_fallueb) {
+        if (ce == R.id.btn_speichern_fallueb) {
            /*
             //werden die Buttons eingeblendet
             lnl_buttons.setVisibility(View.VISIBLE);
@@ -124,13 +176,6 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
             lnl_buttons.invalidate();
             */
         }
-
-    }
-    //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //Aufruf der Prozedur mit Übergabe der Position des geklickten Items/Menüpunkt
-        selectItemFromDrawer(position);
     }
 
     private void selectItemFromDrawer(int position){
@@ -161,5 +206,20 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
             startActivity(intent);
         }
 
+    }
+    //Nicolas Simon, übernommen von Vivien Stumpe, 17.04.16
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //Aufruf der Prozedur mit Übergabe der Position des geklickten Items/Menüpunkt
+        selectItemFromDrawer(position);
+    }
+    /* von Vivien Stumpe, 21.05.16
+    Prozedur, die die Select Anweisungen zum ausgewählten Fall ausführt,
+    damit die gespeicherten Werte in den Screens angezeigt werden können
+    !FOLGT!
+     */
+
+    public void ladeFallDaten(int position){
+        Log.d("Fall","Fall "+faelleArray[position]);
     }
 }
