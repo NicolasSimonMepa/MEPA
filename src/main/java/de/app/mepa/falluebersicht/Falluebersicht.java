e15a57fed8e85e4c05b0338ba77baa25a1403dc2
vstumpe <vivien.stumpe@t-online.de>
2016-05-27 14:21:21 +0200

    Update Falluebersicht.java
    Laden der Falldaten wenn ein Fall angeklickt wird

--- src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
+++ src/main/java/de/app/mepa/falluebersicht/Falluebersicht.java
@@ -1,4 +1,4 @@
-//Zuletzt geändert von Vivien Stumpe 23.05.16
+//Zuletzt geändert von Vivien Stumpe 27.05.16
 package de.app.mepa.falluebersicht;
 
 
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -78,7 +79,8 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
     Adapter_Falluebersicht faelleAdapter;
     FalleingabeDataSource dataSource;
     GlobaleDaten mfall;
-    GlobaleDaten mfalldaten;
+    GlobaleDaten mfalldaten, mpat;
+    String[] patList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,15 +135,17 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
                 Intent fall = new Intent(Falluebersicht.this, Falleingabe.class);
                 // von Vivien Stumpe, 23.05.16
                 //Teilt den String bei einem Leerzeichen
-                String string = faelleArray[position];
+                String string = patList[position];
                 String[] parts = string.split("\\s+");
                 String prot_id_string = parts[0];
+
+                Log.d("Fall", prot_id_string + " Übergebene Fall-ID aus der Listview");
                 String name = parts[1];
+                Log.d("Fall", name + " Übergebener Name aus der Listview");
                 //prot_id enthält die ID des ausgewählten Falls
                 int prot_id = Integer.parseInt(prot_id_string);
                ladeFallDaten(prot_id);
                 startActivity(fall);
-               Log.d("Fall", prot_id + " ID");
             }
         });
 
@@ -171,7 +175,7 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
         */
     }
     public void showAllListEntriesNeu () {
-        String[] patList = dataSource.getAllPat();
+        patList = dataSource.getAllPat();
 
         faelleAdapter =
                 new Adapter_Falluebersicht(
@@ -272,19 +276,174 @@ public class Falluebersicht extends AppCompatActivity implements View.OnClickLis
     /* von Vivien Stumpe, 21.05.16
     Prozedur, die die Select Anweisungen zum ausgewählten Fall ausführt,
     damit die gespeicherten Werte in den Screens angezeigt werden können
-    !FOLGT!
+
      */
     /* von Vivien Stumpe, 23.05.16
     Prozedur, die die Daten zum Fall lädt
     */
     public void ladeFallDaten(int id){
         dataSource.open();
-        dataSource.selectFall(id);
+        mfalldaten=dataSource.selectFall(id);
+        mpat=dataSource.selectPat(id);
+        patientendatenSpeichern(mpat);
         mfall=(GlobaleDaten)getApplication();
         // Hier würden eigentlich die Falldaten geladen
-        mfalldaten=dataSource.selectFall(id);
-        mfall.setPat_name(mfalldaten.getPat_name());
-        mfall.setMas_betreuung(mfalldaten.getMas_betreuung());
+       mfalldaten=dataSource.selectFall(id);
+        fallDatenSpeichern(mfalldaten);
+    }
+    /* von Vivien Stumpe, 27.05.16
+    Prozedur, die die Patientendaten aus der DB zwischenspeichert
+    damit sie im Screen angezeigt werden können
+     */
+    private void patientendatenSpeichern(GlobaleDaten mpatient){
+        mfall=(GlobaleDaten)getApplication();
+        mfall.setPat_name(mpatient.getPat_name());
+        mfall.setPat_vorname(mpatient.getPat_vorname());
+        mfall.setPat_geb(mpatient.getPat_geb());
+        mfall.setPat_sex(mpatient.getPat_sex());
+        mfall.setPat_tel(mpatient.getPat_tel());
+        mfall.setPat_str(mpatient.getPat_str());
+        mfall.setPat_plz(mpatient.getPat_plz());
+        mfall.setPat_ort(mpatient.getPat_ort());
+        mfall.setPat_land(mpatient.getPat_land());
+        mfall.setPat_IDm(mpatient.getPatID());
+        mfall.setPat_krankenkasse(mpatient.getPat_krankenkasse());
+        mfall.setPat_versichertennr(mpatient.getPat_versichertennr());
+        mfall.setPat_versnr(mpatient.getPat_versnr());
+        mfall.setPat_sani(mpatient.getPat_sani());
+        mfall.setEin_ID(mpatient.getFallID());
+    }
+    /*  von Vivien Stumpe, 27.05.16
+        Prozedur, die die Daten aus der DB zum gewählten Fall übernimmt
+        und diese dann zwischenspeichert,
+        damit sie im Screen angezeigt werden
+     */
+    private void fallDatenSpeichern(GlobaleDaten meinsatz){
+        mfall=(GlobaleDaten)getApplication();
+        //Verletzung
+        mfall.setVerl_elektrounfall(meinsatz.getVerl_elektrounfall());
+        mfall.setVerl_wunde_verletzung(meinsatz.getVerl_wunde_verletzung());
+        mfall.setVerl_inhalationstrauma(meinsatz.getVerl_inhalationstrauma());
+        mfall.setVerl_sonstiges(meinsatz.getVerl_sonstiges());
+        mfall.setVerl_verbrennung(meinsatz.getVerl_verbrennung());
+        mfall.setVerl_prellung_verletzung(meinsatz.getVerl_prellung_verletzung());
+        mfall.setVerl_schaedel_art(meinsatz.getVerl_schaedel_art());
+        mfall.setVerl_gesicht_art(meinsatz.getVerl_gesicht_art());
+        mfall.setVerl_brustkorb_art(meinsatz.getVerl_brustkorb_art());
+        mfall.setVerl_bws_lws_art(meinsatz.getVerl_bws_lws_art());
+        mfall.setVerl_hws_art(meinsatz.getVerl_hws_art());
+        mfall.setVerl_becken_art(meinsatz.getVerl_becken_art());
+        mfall.setVerl_bauch_art(meinsatz.getVerl_bauch_art());
+        mfall.setVerl_beine_art(meinsatz.getVerl_beine_art());
+        mfall.setVerl_arme_art(meinsatz.getVerl_arme_art());
+        mfall.setVerl_weichteile_art(meinsatz.getVerl_weichteile_art());
+        mfall.setVerl_schaedel_grad(meinsatz.getVerl_schaedel_grad());
+        mfall.setVerl_gesicht_grad(meinsatz.getVerl_gesicht_grad());
+        mfall.setVerl_brustkorb_grad(meinsatz.getVerl_brustkorb_grad());
+        mfall.setVerl_bws_lws_grad(meinsatz.getVerl_bws_lws_grad());
+        mfall.setVerl_hws_grad(meinsatz.getVerl_hws_grad());
+        mfall.setVerl_becken_grad(meinsatz.getVerl_becken_grad());
+        mfall.setVerl_bauch_grad(meinsatz.getVerl_bauch_grad());
+        mfall.setVerl_beine_grad(meinsatz.getVerl_beine_grad());
+        mfall.setVerl_arme_grad(meinsatz.getVerl_arme_grad());
+        mfall.setVerl_weichteile_grad(meinsatz.getVerl_weichteile_grad());
 
+        //Erkrankung/Vergiftung
+/* Fehlermeldung:
+Caused by: java.lang.NullPointerException
+                                                       at de.app.mepa.GlobaleDaten.Erk_eingabe(GlobaleDaten.java:636)
+                                                       at de.app.mepa.falleingabe.Falleingabe.IconsSave(Falleingabe.java:471)
+                                                       at de.app.mepa.falleingabe.Falleingabe.onCreate(Falleingabe.java:291)
+        mfall.setErk_atmung(meinsatz.getErk_atmung());
+        mfall.setErk_herzkreislauf(meinsatz.getErk_herzkreislauf());
+        mfall.setErk_baucherkrankung(meinsatz.getErk_baucherkrankung());
+        mfall.setErk_stoffwechsel(meinsatz.getErk_stoffwechsel());
+        mfall.setErk_hitzeschlag(meinsatz.getErk_hitzeschlag());
+        mfall.setErk_vergiftung(meinsatz.getErk_vergiftung());
+        mfall.setErk_unterkuehlung(meinsatz.getErk_unterkuehlung());
+        mfall.setErk_gynaekologie(meinsatz.getErk_gynaekologie());
+        mfall.setErk_geburtshilfe(meinsatz.getErk_geburtshilfe());
+        mfall.setErk_hitzeerschoepfung(meinsatz.getErk_hitzeerschoepfung());
+        mfall.setErk_kindernotfall(meinsatz.getErk_kindernotfall());
+        mfall.setErk_neurologie(meinsatz.getErk_neurologie());
+        mfall.setErk_psychatrie(meinsatz.getErk_psychatrie());
+        mfall.setErk_alkoholisiert(meinsatz.getErk_alkoholisiert());
+        mfall.setErk_sonstiges(meinsatz.getErk_sonstiges());
+        mfall.setErk_edtxt_sonstiges(meinsatz.getErk_edtxt_sonstiges());
+        mfall.setErk_schwindel(meinsatz.getErk_schwindel());
+        mfall.setErk_erbrechen(meinsatz.getErk_erbrechen());
+*/
+        //Maßnahmen
+        mfall.setMas_stb_seitenlage(meinsatz.getMas_stb_seitenlage());
+        mfall.setMas_oberkoerperhochlage(meinsatz.getMas_oberkoerperhochlage());
+        mfall.setMas_flachlagerung(meinsatz.getMas_flachlagerung());
+        mfall.setMas_schocklagerung(meinsatz.getMas_schocklagerung());
+        mfall.setMas_vakuummatratze(meinsatz.getMas_vakuummatratze());
+        mfall.setMas_hws_stuetzkragen(meinsatz.getMas_hws_stuetzkragen());
+        mfall.setMas_medikamente(meinsatz.getMas_medikamente());
+        mfall.setMas_extr_schienung(meinsatz.getMas_extr_schienung());
+        mfall.setMas_wundversorgung(meinsatz.getMas_wundversorgung());
+        mfall.setMas_ekg(meinsatz.getMas_ekg());
+        mfall.setMas_ven_zugang(meinsatz.getMas_ven_zugang());
+        mfall.setMas_infusion(meinsatz.getMas_infusion());
+        mfall.setMas_atemwege_freim(meinsatz.getMas_atemwege_freim());
+        mfall.setMas_notkompetenz(meinsatz.getMas_notkompetenz());
+        mfall.setMas_sauerstoffgabe(meinsatz.getMas_sauerstoffgabe());
+        mfall.setMas_intubation(meinsatz.getMas_intubation());
+        mfall.setMas_beatmung(meinsatz.getMas_beatmung());
+        mfall.setMas_herzdruckmassage(meinsatz.getMas_herzdruckmassage());
+        mfall.setMas_erstdefibrillation(meinsatz.getMas_erstdefibrillation());
+        mfall.setMas_betreuung(meinsatz.getMas_betreuung());
+        mfall.setMas_sonstiges(meinsatz.getMas_sonstiges());
+        mfall.setMas_sonstiges_text(meinsatz.getMas_sonstiges_text());
+        mfall.setMas_keine(meinsatz.getMas_keine());
+
+        //Erstbefund
+        mfall.setErst_bewusstsein(meinsatz.getErst_bewusstsein());
+        mfall.setErst_schmerzen(meinsatz.getErst_schmerzen());
+        mfall.setErst_kreislauf(meinsatz.getErst_kreislauf());
+        mfall.setErst_ekg(meinsatz.getErst_ekg());
+        mfall.setErst_atmung(meinsatz.getErst_atmung());
+        mfall.setErst_rr_sys(meinsatz.getErst_rr_sys());
+        mfall.setErst_rr_dia(meinsatz.getErst_rr_dia());
+        mfall.setErst_puls(meinsatz.getErst_puls());
+        mfall.setErst_af(meinsatz.getErst_af());
+        mfall.setErst_spo(meinsatz.getErst_spo());
+        mfall.setErst_bz(meinsatz.getErst_bz());
+        mfall.setErst_pupille_li(meinsatz.getErst_pupille_li());
+        mfall.setErst_pupille_re(meinsatz.getErst_pupille_re());
+
+        //Übergabe
+        //Ergebnis_zeit?
+        mfall.setErg_wertsachen(meinsatz.getErg_wertsachen());
+        mfall.setBem_bemerkung(meinsatz.getBem_bemerkung());
+        mfall.setErg_funkruf(meinsatz.getErg_funkruf());
+        mfall.setErg_transport(meinsatz.getErg_transport());
+        mfall.setErg_transport_ziel(meinsatz.getErg_transport_ziel());
+        mfall.setErg_entlassung_ev(meinsatz.getErg_entlassung_ev());
+        mfall.setErg_zeuge(meinsatz.getErg_zeuge());
+        mfall.setErg_zustand(meinsatz.getErg_zustand());
+        mfall.setErg_notarzt(meinsatz.getErg_notarzt());
+        mfall.setErg_hausarzt_informiert(meinsatz.getErg_hausarzt_informiert());
+        mfall.setErg_tod(meinsatz.getErg_tod());
+        mfall.setErg_transport_sonstiges(meinsatz.getErg_transport_sonstiges());
+        mfall.setErg_ersthelfermassn(meinsatz.getErg_ersthelfermassn());
+        mfall.setErg_nachforderung_ktw(meinsatz.getErg_nachforderung_ktw());
+        mfall.setErg_nachforderung_rtw(meinsatz.getErg_nachforderung_rtw());
+        mfall.setErg_nachforderung_nef(meinsatz.getErg_nachforderung_nef());
+        mfall.setErg_nachforderung_naw(meinsatz.getErg_nachforderung_naw());
+        mfall.setErg_nachforderung_rth(meinsatz.getErg_nachforderung_rth());
+        mfall.setErg_nachforderung_feuerwehr(meinsatz.getErg_nachforderung_feuerwehr());
+        mfall.setErg_nachforderung_polizei(meinsatz.getErg_nachforderung_polizei());
+
+        //Einsatz
+        mfall.setEin_mosan(meinsatz.getEin_mosan());
+        mfall.setEin_sanw(meinsatz.getEin_sanw());
+        mfall.setEin_hilfs(meinsatz.getEin_hilfs());
+        mfall.setEin_zugef(meinsatz.getEin_zugef());
+        mfall.setEin_fundort(meinsatz.getEin_fundort());
+        mfall.setNotf_notfallsituation(meinsatz.getNotf_notfallsituation());
+
+        //Stammdaten??
     }
 }
